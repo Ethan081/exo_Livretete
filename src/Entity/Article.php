5d25f3dbbdf7add9a82ec7076a350ca8d9aa7879
zemac <etienne.ziegelmeyer@lapiscine.pro>
2019-07-12 17:29:11 +0200

    add columns author category description date

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -16,4 +16,26 @@ class Article
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="string", name="author")
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="string", name="category")
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="text", name="description")
+     */
+    private $decription;
+
+    //je cree une nouvelle column dans dans ma table Article.
+    /**
+     * @ORM\Column(type="date", name="date_of_publication")
+     */
+    private $date_of_publication;
+
 }
---
+++ src/Migrations/Version20190712151241.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190712151241 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article ADD description LONGTEXT NOT NULL, ADD author VARCHAR(255) NOT NULL, ADD category VARCHAR(255) NOT NULL, ADD date_of_publication DATE NOT NULL, DROP title');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article ADD title VARCHAR(50) NOT NULL COLLATE utf8mb4_unicode_ci, DROP description, DROP author, DROP category, DROP date_of_publication');
+    }
+}
